797ff618210d228d84cb22428cf2b1344f202f84
Daniil Ovchinnikov <Daniil.Ovchinnikov@jetbrains.com>
2014-07-09 17:20:19 +0400

    Test dummy update

--- test/com/jetbrains/plugins/checkerframework/configurable/ClassScannerTest.java
+++ test/com/jetbrains/plugins/checkerframework/configurable/ClassScannerTest.java
@@ -3,14 +3,10 @@ package com.jetbrains.plugins.checkerframework.configurable;
 import org.checkerframework.javacutil.AbstractTypeProcessor;
 import org.junit.Test;
 
-/**
- * @author Daniil Ovchinnikov.
- * @since 7/8/14.
- */
 public class ClassScannerTest {
 
     @Test
     public void test() {
-        System.out.println(ClassScanner.findChildren(AbstractTypeProcessor.class));
+        System.out.println(ClassScanner.findChildren(AbstractTypeProcessor.class, null));
     }
 }
