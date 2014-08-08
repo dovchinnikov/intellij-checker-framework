7647cc0eaff286df0ee6f1446d66b353e6ccb727
Daniil Ovchinnikov <Daniil.Ovchinnikov@jetbrains.com>
2014-08-08 13:31:40 +0400

    Anootation owner search logic moved to separate util class

--- src/com/jetbrains/plugins/checkerframework/inspection/fix/WrapWithAsRegexFix.java
+++ src/com/jetbrains/plugins/checkerframework/inspection/fix/WrapWithAsRegexFix.java
@@ -5,9 +5,10 @@ import com.intellij.codeInsight.daemon.QuickFixBundle;
 import com.intellij.codeInspection.LocalQuickFixOnPsiElement;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.*;
-import com.intellij.psi.util.PsiTreeUtil;
 import com.jetbrains.plugins.checkerframework.service.Stuff;
+import com.jetbrains.plugins.checkerframework.util.ExpectedTypesUtil;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public class WrapWithAsRegexFix extends LocalQuickFixOnPsiElement {
 
@@ -31,41 +32,20 @@ public class WrapWithAsRegexFix extends LocalQuickFixOnPsiElement {
     }
 
     @Override
-    public void invoke(@NotNull Project project, @NotNull PsiFile file, @NotNull PsiElement expression, @NotNull PsiElement ignored) {
+    public void invoke(@NotNull Project project, @NotNull PsiFile file, @NotNull PsiElement startElement, @NotNull PsiElement ignored) {
         if (!FileModificationService.getInstance().prepareFileForWrite(file)) {
             return;
         }
-        final @NotNull PsiAnnotationOwner requiredTypePsi;
+        final PsiExpression expression = (PsiExpression)startElement;
         final int numberOfRegexGroups;
         {
-            PsiLocalVariable variable = PsiTreeUtil.getNonStrictParentOfType(expression, PsiLocalVariable.class);
-            if (variable != null) {
-                requiredTypePsi = variable.getType();
-            } else {
-                PsiAssignmentExpression assignmentExpression = PsiTreeUtil.getNonStrictParentOfType(
-                    expression,
-                    PsiAssignmentExpression.class
-                );
-                if (assignmentExpression != null) {
-                    assert assignmentExpression.getLExpression().getType() != null : "Assignment left expression type is null";
-                    requiredTypePsi = assignmentExpression.getLExpression().getType();
-                } else {
-                    assert expression.getContext() instanceof PsiReturnStatement;
-                    PsiMethod method = PsiTreeUtil.getNonStrictParentOfType(expression, PsiMethod.class);
-                    assert method != null : "Cannot find type";
-                    requiredTypePsi = method.getModifierList();
-                }
-            }
-        }
-        {
-            final PsiAnnotation regexAnnotation = requiredTypePsi.findAnnotation(Stuff.REGEX_ANNO_FQN);
+            final @NotNull PsiAnnotationOwner expectedTypeAnnotationOwner = ExpectedTypesUtil.findAnnotationOwner(expression);
+            final @Nullable PsiAnnotation regexAnnotation = expectedTypeAnnotationOwner.findAnnotation(Stuff.REGEX_ANNO_FQN);
             if (regexAnnotation != null) {
-                final PsiAnnotationMemberValue annotationMemberValue = regexAnnotation.findAttributeValue("value");
-                if (annotationMemberValue != null) {
-                    numberOfRegexGroups = Integer.parseInt(annotationMemberValue.getText());
-                } else {
-                    return;
-                }
+                final PsiAnnotationMemberValue annotationMemberValue =
+                    regexAnnotation.findAttributeValue(PsiAnnotation.DEFAULT_REFERENCED_METHOD_NAME);
+                assert annotationMemberValue != null;
+                numberOfRegexGroups = Integer.parseInt(annotationMemberValue.getText());
             } else {
                 return;
             }
---
+++ src/com/jetbrains/plugins/checkerframework/util/ExpectedTypesUtil.java
@@ -0,0 +1,68 @@
+package com.jetbrains.plugins.checkerframework.util;
+
+import com.intellij.codeInsight.ExpectedTypeUtil;
+import com.intellij.psi.*;
+import com.intellij.psi.util.PsiTreeUtil;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class ExpectedTypesUtil {
+
+    @Nullable
+    public static PsiType findInAssignmentExpression(@NotNull PsiExpression expression) {
+        final PsiAssignmentExpression assignmentExpression = PsiTreeUtil.getNonStrictParentOfType(
+            expression,
+            PsiAssignmentExpression.class
+        );
+        if (assignmentExpression != null) {
+            assert assignmentExpression.getLExpression().getType() != null : "Assignment found but type of left expression is null";
+            return assignmentExpression.getLExpression().getType();
+        } else {
+            return null;
+        }
+    }
+
+    @Nullable
+    public static PsiType findInDeclaration(@NotNull PsiExpression expression) {
+        final PsiLocalVariable variable = PsiTreeUtil.getNonStrictParentOfType(expression, PsiLocalVariable.class);
+        return variable != null ? variable.getType() : null;
+    }
+
+    @Nullable
+    public static PsiType findInMethodCall(@NotNull PsiExpression expression) {
+        final PsiMethodCallExpression methodCallExpression = PsiTreeUtil.getParentOfType(expression, PsiMethodCallExpression.class);
+        if (methodCallExpression == null) return null;
+        int index = -1;
+        {
+            final PsiExpression[] expressions = methodCallExpression.getArgumentList().getExpressions();
+            for (int i = 0; i < expressions.length; i++) {
+                if (expressions[i].equals(expression)) {
+                    index = i;
+                }
+            }
+        }
+        if (index == -1) return null;
+        final PsiMethod method = methodCallExpression.resolveMethod();
+        if (method == null) return null;
+        final PsiSubstitutor substitutor = ExpectedTypeUtil.inferSubstitutor(method, methodCallExpression, false);
+        if (substitutor == null) return null;
+        return method.getSignature(substitutor).getParameterTypes()[index];
+    }
+
+    @Nullable
+    public static PsiModifierList findInReturn(@NotNull PsiExpression expression) {
+        final PsiMethod method = PsiTreeUtil.getParentOfType(expression, PsiMethod.class);
+        return method != null ? method.getModifierList() : null;
+    }
+
+    @NotNull
+    public static PsiAnnotationOwner findAnnotationOwner(@NotNull PsiExpression expression) {
+        PsiAnnotationOwner result = findInDeclaration(expression);
+        if (result == null) result = findInAssignmentExpression(expression);
+        if (result == null) result = findInDeclaration(expression);
+        if (result == null) result = findInReturn(expression);
+        if (result == null) result = findInMethodCall(expression);
+        assert result != null;
+        return result;
+    }
+}
