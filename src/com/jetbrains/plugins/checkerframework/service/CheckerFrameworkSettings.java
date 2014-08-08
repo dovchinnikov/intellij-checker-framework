cd3ee53197a7e5dbc935a6b0b66f8198026b4df9
Daniil Ovchinnikov <Daniil.Ovchinnikov@jetbrains.com>
2014-08-08 16:36:32 +0400

    CF settings state moved to separate file

--- src/com/jetbrains/plugins/checkerframework/service/CheckerFrameworkSettings.java
+++ src/com/jetbrains/plugins/checkerframework/service/CheckerFrameworkSettings.java
@@ -34,7 +34,7 @@ import java.util.jar.Manifest;
         )
     }
 )
-public class CheckerFrameworkSettings implements PersistentStateComponent<CheckerFrameworkSettings.State> {
+public class CheckerFrameworkSettings implements PersistentStateComponent<CheckerFrameworkSettingsState> {
 
     private static final Logger            LOG                        = Logger.getInstance(CheckerFrameworkSettings.class);
     private static final PluginClassLoader CURRENT_PLUGIN_CLASSLOADER = (PluginClassLoader)CheckerFrameworkSettings.class.getClassLoader();
@@ -42,8 +42,9 @@ public class CheckerFrameworkSettings implements PersistentStateComponent<Checke
     private final @NotNull List<String>                     myCheckers       = new ArrayList<String>();
     private final @NotNull List<Class<? extends Processor>> myCheckerClasses = new ArrayList<Class<? extends Processor>>();
 
-    private @NotNull State   myState    = new State();
-    private          boolean needReload = true;
+    private @NotNull CheckerFrameworkSettingsState myState = new CheckerFrameworkSettingsState();
+
+    private boolean needReload = true;
     private Method myAggregateProcessorFactoryMethod;
     private String myErrorMessage;
 
@@ -128,14 +129,14 @@ public class CheckerFrameworkSettings implements PersistentStateComponent<Checke
 
     @NotNull
     @Override
-    public State getState() {
+    public CheckerFrameworkSettingsState getState() {
         return myState;
     }
 
     @Override
-    public void loadState(final State state) {
+    public void loadState(final CheckerFrameworkSettingsState state) {
         needReload = needReload || !state.pathToCheckerJar.equals(state.pathToCheckerJar);
-        myState = new State(state);
+        myState = new CheckerFrameworkSettingsState(state);
     }
 
     @Override
@@ -275,69 +276,5 @@ public class CheckerFrameworkSettings implements PersistentStateComponent<Checke
         myCheckers.clear();
         myCheckers.addAll(myState.builtInCheckers);
         myCheckers.addAll(myState.customCheckers);
-    }
-
-    public static class State {
-        public @NotNull String       pathToCheckerJar;
-        public @NotNull Set<String>  builtInCheckers;
-        public @NotNull Set<String>  customCheckers;
-        public @NotNull Set<String>  enabledCheckers;
-        public @NotNull List<String> options;
-        public @NotNull String       version;
-        public          boolean      valid;
-
-        public State() {
-            pathToCheckerJar = "";
-            builtInCheckers = new HashSet<String>();
-            customCheckers = new HashSet<String>();
-            enabledCheckers = new HashSet<String>();
-            options = new ArrayList<String>();
-            version = "";
-            valid = false;
-        }
-
-        public State(@NotNull State state) {
-            pathToCheckerJar = state.pathToCheckerJar;
-            builtInCheckers = new HashSet<String>(state.builtInCheckers);
-            customCheckers = new HashSet<String>(state.customCheckers);
-            enabledCheckers = new HashSet<String>(state.enabledCheckers);
-            options = new ArrayList<String>(state.options);
-            version = state.version;
-            valid = state.valid;
-        }
-
-        private static <T> boolean collectionEquals(@NotNull Collection<T> a, @NotNull Collection<T> b) {
-            return a.containsAll(b) && b.containsAll(a);
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) {
-                return true;
-            }
-            if (o == null || getClass() != o.getClass()) {
-                return false;
-            }
-            final State state = (State)o;
-            return valid == state.valid
-                   && version.equals(version)
-                   && pathToCheckerJar.equals(state.pathToCheckerJar)
-                   && collectionEquals(builtInCheckers, state.builtInCheckers)
-                   && collectionEquals(customCheckers, state.customCheckers)
-                   && collectionEquals(enabledCheckers, state.enabledCheckers)
-                   && collectionEquals(options, state.options);
-        }
-
-        @Override
-        public int hashCode() {
-            int result = pathToCheckerJar.hashCode();
-            result = 31 * result + builtInCheckers.hashCode();
-            result = 31 * result + customCheckers.hashCode();
-            result = 31 * result + enabledCheckers.hashCode();
-            result = 31 * result + options.hashCode();
-            result = 31 * result + version.hashCode();
-            result = 31 * result + (valid ? 1 : 0);
-            return result;
-        }
     }
 }
---
+++ src/com/jetbrains/plugins/checkerframework/service/CheckerFrameworkSettingsState.java
@@ -0,0 +1,69 @@
+package com.jetbrains.plugins.checkerframework.service;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.*;
+
+public class CheckerFrameworkSettingsState {
+    public @NotNull String       pathToCheckerJar;
+    public @NotNull Set<String>  builtInCheckers;
+    public @NotNull Set<String>  customCheckers;
+    public @NotNull Set<String>  enabledCheckers;
+    public @NotNull List<String> options;
+    public @NotNull String       version;
+    public          boolean      valid;
+
+    public CheckerFrameworkSettingsState() {
+        pathToCheckerJar = "";
+        builtInCheckers = new HashSet<String>();
+        customCheckers = new HashSet<String>();
+        enabledCheckers = new HashSet<String>();
+        options = new ArrayList<String>();
+        version = "";
+        valid = false;
+    }
+
+    public CheckerFrameworkSettingsState(@NotNull CheckerFrameworkSettingsState state) {
+        pathToCheckerJar = state.pathToCheckerJar;
+        builtInCheckers = new HashSet<String>(state.builtInCheckers);
+        customCheckers = new HashSet<String>(state.customCheckers);
+        enabledCheckers = new HashSet<String>(state.enabledCheckers);
+        options = new ArrayList<String>(state.options);
+        version = state.version;
+        valid = state.valid;
+    }
+
+    private static <T> boolean collectionEquals(@NotNull Collection<T> a, @NotNull Collection<T> b) {
+        return a.containsAll(b) && b.containsAll(a);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        final CheckerFrameworkSettingsState state = (CheckerFrameworkSettingsState)o;
+        return valid == state.valid
+               && version.equals(version)
+               && pathToCheckerJar.equals(state.pathToCheckerJar)
+               && collectionEquals(builtInCheckers, state.builtInCheckers)
+               && collectionEquals(customCheckers, state.customCheckers)
+               && collectionEquals(enabledCheckers, state.enabledCheckers)
+               && collectionEquals(options, state.options);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = pathToCheckerJar.hashCode();
+        result = 31 * result + builtInCheckers.hashCode();
+        result = 31 * result + customCheckers.hashCode();
+        result = 31 * result + enabledCheckers.hashCode();
+        result = 31 * result + options.hashCode();
+        result = 31 * result + version.hashCode();
+        result = 31 * result + (valid ? 1 : 0);
+        return result;
+    }
+}
