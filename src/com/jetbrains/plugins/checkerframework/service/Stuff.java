3e4843464090d93ccd386663ec711c5ad1e83c31
Daniil Ovchinnikov <Daniil.Ovchinnikov@jetbrains.com>
2014-08-29 15:36:13 +0400

    All builtin checkers listed

--- src/com/jetbrains/plugins/checkerframework/service/Stuff.java
+++ src/com/jetbrains/plugins/checkerframework/service/Stuff.java
@@ -1,11 +1,22 @@
 package com.jetbrains.plugins.checkerframework.service;
 
 import org.checkerframework.checker.fenum.FenumChecker;
+import org.checkerframework.checker.formatter.FormatterChecker;
+import org.checkerframework.checker.guieffect.GuiEffectChecker;
 import org.checkerframework.checker.i18n.I18nChecker;
+import org.checkerframework.checker.igj.IGJChecker;
+import org.checkerframework.checker.interning.InterningChecker;
+import org.checkerframework.checker.javari.JavariChecker;
+import org.checkerframework.checker.linear.LinearChecker;
 import org.checkerframework.checker.lock.LockChecker;
 import org.checkerframework.checker.nullness.NullnessChecker;
+import org.checkerframework.checker.propkey.PropertyKeyChecker;
 import org.checkerframework.checker.regex.RegexChecker;
+import org.checkerframework.checker.signature.SignatureChecker;
+import org.checkerframework.checker.tainting.TaintingChecker;
 import org.checkerframework.checker.units.UnitsChecker;
+import org.checkerframework.common.subtyping.SubtypingChecker;
+import org.checkerframework.common.value.ValueChecker;
 import org.checkerframework.framework.source.SourceChecker;
 import org.checkerframework.framework.util.CheckerMain;
 
@@ -14,13 +25,6 @@ import java.util.List;
 
 public interface Stuff {
 
-    //    String MANIFEST_VERSION_KEY      = "Implementation-Version";
-    // TODO should be 1.8.5, set to 1.8.4 for development purposes
-//    String MINIMUM_SUPPORTED_VERSION = "1.8.4";
-//    String CHECKERS_BASE_CLASS_FQN   = "org.checkerframework.framework.source.SourceChecker";
-//    String CHECKERS_PACKAGE          = "org.checkerframework.checker";
-//    String AGGREGATE_PROCESSOR_FQN   = "com.jetbrains.plugins.checkerframework.util.AggregateCheckerEx";
-//    String COMPILER_IMPL_FQN         = "CheckerFrameworkCompilerImpl";
     String REGEX_ANNO_FQN  = "org.checkerframework.checker.regex.qual.Regex";
     String REGEX_UTIL_FQN  = "org.checkerframework.checker.regex.RegexUtil";
     String PATH_TO_CHECKER = CheckerMain.findPathTo(SourceChecker.class, false);
@@ -29,10 +33,22 @@ public interface Stuff {
 
     List<Class<? extends SourceChecker>> BUILTIN_CHECKERS = Arrays.asList(
         NullnessChecker.class,
-        FenumChecker.class,
-        RegexChecker.class,
-        UnitsChecker.class,
+        InterningChecker.class,
         LockChecker.class,
-        I18nChecker.class
+        FenumChecker.class,
+        TaintingChecker.class,
+        RegexChecker.class,
+        FormatterChecker.class,
+        PropertyKeyChecker.class,
+        SignatureChecker.class,
+        GuiEffectChecker.class,
+        UnitsChecker.class,
+        LinearChecker.class,
+        IGJChecker.class,
+        JavariChecker.class,
+        SubtypingChecker.class,
+        LockChecker.class,
+        I18nChecker.class,
+        ValueChecker.class
     );
 }
